1e133009f8abbc7b07f625a9794d7fbf157f695d
Jarogna <>
2021-04-26 18:21:52 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// A Football Scoreboard program.
+// Teacher: Dr. Tyson McMillian
+// Student: Ronald Angora
+// Date: 04/26/2021
+
+#include <iostream>
+#include <string> 
+#include <unistd.h>
+#include "Input_Validation_Extended.h"
+using namespace std; 
+
+int main() {
+  std::cout << "Hello World!\n";
+}
