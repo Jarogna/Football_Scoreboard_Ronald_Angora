b64da7f97411b652c4b0d2a31252a38a1f0d64f9
Jarogna <>
2021-04-26 19:50:50 +0000

    Moved the classes to Football_Scoreboard_Classes.h file

---
+++ Football_Scoreboard_Classes.h
@@ -0,0 +1,82 @@
+// Class 'Team' and 'Scoreboard' for the Football Scoreboard program
+// Teacher: Dr. Tyson McMillian
+// Student: Ronald Angora
+// Date: 04/26/2021
+
+class Team
+{
+  private:
+    string name;
+    string coachName;
+    string homeCity;
+    bool homeStatus;
+    int score;
+    int timeoutCount;
+
+  public:
+    Team() // Default constructor
+    {
+      name = "DefaultTeamName";
+      coachName = "DefaultCoachName";
+      homeCity = "DefaultHomeCity";
+      homeStatus = false; // Home = True, Visitor = false
+      score = 0;
+      timeoutCount = 3;
+    }
+    // Setters
+    void setName(string n) { name = n; }
+    void setCoachName(string sCN) { coachName = sCN; }
+    void setHomecity(string sHC) { homeCity = sHC; }
+    void setHomeStatus(bool hs) { homeStatus = hs; }
+    void setScore(int s) { score = s; }
+    void setTimeoutCount(int tc) { timeoutCount = tc; }
+    // Getters
+    string getName() const { return name; }
+    string getCoachName() const { return coachName; }
+    string getHomeCity() const { return homeCity; }
+    bool getHomeStatus() const { return homeStatus; }
+    int getScore() const { return score; }
+    int getTimeoutCount() const { return timeoutCount; }
+};
+
+class Scoreboard
+{
+  private:
+    int half = 0;
+    Team home;
+    Team visitor;
+  public:
+    Scoreboard() // Default Constructor
+    {
+      half = 0;
+    }
+    // Setters
+    void setHalf(int h) { half = h; }
+    void setHome(Team hSet) { home = hSet; }
+    void setVisitor(Team vSet) { visitor = vSet; }
+    // Getters
+    int getHalf() const { return half; }
+    Team getHome() const { return home; }
+    Team getVisitor() const { return visitor; }
+
+    void showScoreboard()
+    {
+      string color = "";
+      string reset = "\x1b[0m";
+      color = "\x1b[37;4m"; // Grey
+      string color2 = "\x1b[39;4m"; // White
+      string score = "\x1b[93;1m"; // Gold
+      cout << color << "*************Scoreboard*************\n" << reset << endl;
+      // Team Names
+      cout << color2 << home.getName() << reset << "\t\t" << color2 << visitor.getName() << reset << endl;
+      // Scores & Timeout Count
+      cout << "\t  " << score << home.getScore() << reset << "\t\t  " << score << home.getTimeoutCount() << reset << "\t\t" << score << visitor.getTimeoutCount() << reset << "\t\t" << score << visitor.getScore() << reset << endl;
+      // Coach Names
+      cout << color2 << home.getCoachName() << reset << "\t" << color2 << visitor.getCoachName() << reset << endl;
+      // City Names
+      cout << color2 << home.getHomeCity() << reset << "\t\t" << color2 << visitor.getHomeCity() << reset << endl;
+      cout << "\n";
+      for(int i = 0; i < 36; i++) { cout << color << "*" << reset; } cout << endl;
+    }
+
+};
--- main.cpp
+++ main.cpp
@@ -7,81 +7,8 @@
 #include <string> 
 #include <unistd.h>
 #include "Input_Validation_Extended.h"
+#include "Football_Scoreboard_Classes.h"
 using namespace std; 
-
-class Team
-{
-  private:
-    string name;
-    string coachName;
-    string homeCity;
-    bool homeStatus;
-    int score;
-    int timeoutCount;
-
-  public:
-    Team() //Default constructor
-    {
-      name = "DefaultTeamName";
-      coachName = "DefaultCoachName";
-      homeCity = "DefaultHomeCity";
-      homeStatus = false; // Home = True, Visitor = false
-      score = 0;
-      timeoutCount = 3;
-    }
-    // Setters
-    void setName(string n) { name = n; }
-    void setCoachName(string sCN) { coachName = sCN; }
-    void setHomecity(string sHC) { homeCity = sHC; }
-    void setHomeStatus(bool hs) { homeStatus = hs; }
-    void setScore(int s) { score = s; }
-    void setTimeoutCount(int tc) { timeoutCount = tc; }
-    // Getters
-    string getName() const { return name; }
-    string getCoachName() const { return coachName; }
-    string getHomeCity() const { return homeCity; }
-    bool getHomeStatus() const { return homeStatus; }
-    int getScore() const { return score; }
-    int getTimeoutCount() const { return timeoutCount; }
-};
-
-class Scoreboard
-{
-  private:
-    int half = 0;
-    Team home;
-    Team visitor;
-  public:
-    Scoreboard()
-    {
-      half = 0;
-    }
-    // Setters
-    void setHalf(int h) { half = h; }
-    void setHome(Team hSet) { home = hSet; }
-    void setVisitor(Team vSet) { visitor = vSet; }
-    // Getters
-    int getHalf() const { return half; }
-    Team getHome() const { return home; }
-    Team getVisitor() const { return visitor; }
-
-    void showScoreboard()
-    {
-      string color = "";
-      string reset = "\x1b[0m";
-      color = "\x1b[40;4m"; // White Bold
-      string score = "\x1b[31;1m"; // Red
-      cout << color << "*************Scoreboard*************" << reset << endl;
-      cout << home.getName() << "\t\t" << visitor.getName() << endl;
-      cout << "\t" << score << home.getScore() << reset << "\t\t\t\t\t\t" << visitor.getScore() << endl;
-      cout << color << home.getCoachName() << reset << "\t" << visitor.getCoachName() << endl;
-      cout << color << home.getHomeCity() << reset << "\t\t" << visitor.getHomeCity() << endl;
-      cout << "\t" << color << home.getTimeoutCount() << reset << "\t\t\t\t\t\t" << visitor.getTimeoutCount() << endl;
-      for(int i = 0; i < 47; i++) { cout << "*"; } cout << endl;
-    }
-
-};
-
 
 int main() 
 {
