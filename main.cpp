a88f88065dac66db23a7ca3a7656ecb805cf30e3
Jarogna <>
2021-04-26 19:23:57 +0000

    Added Class 'Scoreboard'

--- main.cpp
+++ main.cpp
@@ -45,8 +45,55 @@ class Team
     int getTimeoutCount() const { return timeoutCount; }
 };
 
+class Scoreboard
+{
+  private:
+    int half = 0;
+    Team home;
+    Team visitor;
+  public:
+    Scoreboard()
+    {
+      half = 0;
+    }
+    // Setters
+    void setHalf(int h) { half = h; }
+    void setHome(Team hSet) { home = hSet; }
+    void setVisitor(Team vSet) { visitor = vSet; }
+    // Getters
+    int getHalf() const { return half; }
+    Team getHome() const { return home; }
+    Team getVisitor() const { return visitor; }
+
+    void showScoreboard()
+    {
+      string color = "";
+      string reset = "\x1b[0m";
+      color = "\x1b[40;4m"; // White Bold
+      string score = "\x1b[31;1m"; // Red
+      cout << color << "*************Scoreboard*************" << reset << endl;
+      cout << home.getName() << "\t\t" << visitor.getName() << endl;
+      cout << "\t" << score << home.getScore() << reset << "\t\t\t\t\t\t" << visitor.getScore() << endl;
+      cout << color << home.getCoachName() << reset << "\t" << visitor.getCoachName() << endl;
+      cout << color << home.getHomeCity() << reset << "\t\t" << visitor.getHomeCity() << endl;
+      cout << "\t" << color << home.getTimeoutCount() << reset << "\t\t\t\t\t\t" << visitor.getTimeoutCount() << endl;
+      for(int i = 0; i < 47; i++) { cout << "*"; } cout << endl;
+    }
+
+};
+
 
 int main() 
 {
-  
+  Scoreboard s;
+  string userChoice = "";
+
+  do
+  {
+    s.showScoreboard();
+    cin >> userChoice;
+
+  } while (userChoice != "E" && userChoice != "e");
+
+  return 0;
 }
